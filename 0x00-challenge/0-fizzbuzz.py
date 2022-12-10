642c3d6e58aac7583c22a9dd72d619662a6535e0
wondesem <wseminew50gmail.com>
2022-12-09 23:38:57 -0800

    My first commit on task 1

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -20,18 +20,20 @@ def fizzbuzz(n):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
-            tmp_result.append("Fizz")elif (i % 5) == 0:
-                tmp_result.append("Buzz")
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
-    if __name__ == '__main__':
-        if len(sys.argv) <= 1:
-            print("Missing number")
-            print("Usage: ./0-fizzbuzz.py <number>")
-            print("Example: ./0-fizzbuzz.py 89")
-            sys.exit(1)
 
-        number = int(sys.argv[1])
-        fizzbuzz(number)
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
